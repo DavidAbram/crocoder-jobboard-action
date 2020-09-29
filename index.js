6ee7b9b01ba7989253f1d91f228f522b75afb75b
David Abram <david@abram.com>
2020-09-29 15:46:25 +0200

    ls back again

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('AAAAAA');
+const core = require('@actions/core');
+const { exec } = require('@actions/exec');
+
+(async () => {
+  try {
+    await exec('ls');
+  } catch (error) {
+    console.log(error.message);
+    core.setFailed(error.message)
+  }
+})();
