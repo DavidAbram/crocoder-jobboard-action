ae5452a9196e7b98da465659907a3101297433a4
David Abram <david@abram.com>
2021-01-28 20:02:12 +0100

    exec

--- archiveAll.js
+++ archiveAll.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
+const { exec } = require('@actions/exec');
 const { customAlphabet } = require("nanoid");
 const nanoid = customAlphabet(
   "ModuleSymbhasOwnPrABCDEFGHNRVfgctiUvzKqYTJkLxpZXIjQW",
